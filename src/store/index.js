178261d83b4cbc56ffd639bd523f392fa260dfc8
David Royer <droyer01@gmail.com>
2017-08-03 01:56:25 -0400

    Testing auth in browser for safari

--- src/store/index.js
+++ src/store/index.js
@@ -56,7 +56,11 @@ const actions = {
   },
 
   signInFromBrowser({commit}) {
-    window.open('/#/test')
+    let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+    width=0,height=0,left=-1000,top=-1000`;
+
+    window.open('/#/test', 'test', params);
+    // window.open('/#/test', '_blank')
   },
 
   signInWithGithub () {
